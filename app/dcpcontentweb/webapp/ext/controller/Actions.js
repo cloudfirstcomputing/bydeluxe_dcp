22c5c959c37b4b6b556156a956e4c2c293ed2cda
fahadcybertech <fahad.hamsa@cybertech.com>
2025-01-06 17:05:01 +0000

    Content creation mapping changes - incomplete

--- app/dcpcontentweb/webapp/ext/controller/Actions.js
+++ app/dcpcontentweb/webapp/ext/controller/Actions.js
@@ -4,16 +4,24 @@ sap.ui.define([
     "sap/m/MessageBox"
 ], function(MessageToast, coreLibrary, MessageBox) {
     'use strict';
-
+    var sResponsivePaddingClasses = "sapUiResponsivePadding--header sapUiResponsivePadding--content sapUiResponsivePadding--footer";
     return {
         handleProcess: function(oEvent) {
 
-            // MessageToast.show("Custom handler invoked.");
             let aData = [];
             let aSelectedItems = this.editFlow.getView().byId("dcpcontentweb::dcpcontentList--fe::table::dcpcontent::LineItem-innerTable").getSelectedItems();
             for (var i in aSelectedItems) {
                 var oEntry = aSelectedItems[i].getBindingContext().getObject();
-                if (oEntry?.SalesOrder) {
+                if(!oEntry){
+                    MessageBox.error(`No entries selected for posting. Please make a selection and proceed`, {
+                        title: "Error",
+                        contentWidth: "auto",
+                        styleClass: sResponsivePaddingClasses
+                    });
+                    aData = [];
+                    break;
+                }
+                else if (oEntry?.SalesOrder) {
                     MessageBox.error(`Posting is already done for ${oEntry.ApplicationID} - ${oEntry.BookingID}. Sales Order is: ${oEntry.SalesOrder}`, {
                         title: "Error",
                         contentWidth: "auto",
@@ -22,14 +30,23 @@ sap.ui.define([
                     aData = [];
                     break;
                 }
-                else {
-                    aData.push(oEntry);
+                else if(!oEntry?.BookingID){
+                    MessageBox.error(`Retry after selecting Booking ID from table settings`, {
+                        title: "Error",
+                        contentWidth: "auto",
+                        styleClass: sResponsivePaddingClasses
+                    });
+                    aData = [];
+                    break;
+                }
+                else{
+                    aData.push(oEntry.BookingID);
                 }
             }
             if (aData?.length) {
                 var oModel = this.getModel();
                 var oActionODataContextBinding = oModel.bindContext("/processContent(...)");
-                oActionODataContextBinding.setParameter("dcpcontent", aData);
+                oActionODataContextBinding.setParameter("bookingIDs", aData);
                 oActionODataContextBinding.execute().then(
                     function (param) {
                         var oActionContext = oActionODataContextBinding.getBoundContext();
